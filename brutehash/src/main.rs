88927a19cb56888a55b2c155688d4c58de5e8682
eryvia <eryvia.reborn@gmail.com>
2025-12-04 23:24:20 +0100

    updated main function : rust

--- brutehash/src/main.rs
+++ brutehash/src/main.rs
@@ -6,28 +6,41 @@ struct Values {
     pepper: String,
     index: i32,
     starter_hash: Vec<char>,
-    hash: String,
+    hashes: Vec<String>,
 }
 
 fn get_values() -> Values {
     let pepper: String = "cajovna-2025-".to_string();
     let index: i32 = 5;
     let starter_hash: Vec<char> = vec!['a', 'b', 'c', 'd', 'e'];
-    let hash: String =
-        "936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af".to_string();
+    let hashes: Vec<String> = vec![
+        "936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af".to_string(),
+        "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8".to_string(),
+    ];
 
     Values {
         pepper,
         index,
         starter_hash,
-        hash,
+        hashes,
     }
 }
 
 fn main() {
     let info: Values = get_values();
+    let mut curr_index: i32 = 0;
+    let mut curr_value: String = "".to_string();
+    let mut control: bool = true;
 
-    for x in info.starter_hash {
-        println!("{}", x);
+    while control == true {
+        for index in 0..info.index {
+            for x in &info.starter_hash {
+                let mut hasher = Sha256::new();
+                hasher.update(curr_value.as_bytes());
+                let result = hasher.finalize();
+                println!("{}", x);
+                control = false;
+            }
+        }
     }
 }
